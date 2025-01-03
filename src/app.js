e59d524d07901a8190d1ce790ca88bde209fe6d1
Iago Barbosa <iago.luiz@ges.inatel.br>
2025-01-03 10:54:05 -0300

    :bug: fix: Port

--- src/app.js
+++ src/app.js
@@ -5,6 +5,12 @@ const routes = require("./routes");
 
 const app = express();
 
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
 // Middleware
 app.use(express.json());
 
